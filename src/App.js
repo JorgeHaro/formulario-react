17c5069e6cd61dce0d070b733c3b32e85bf2c065
JorgeHaro <jharor@uni.pe>
2025-03-04 15:57:09 -0500

    poadowedwed

--- src/App.js
+++ src/App.js
@@ -24,13 +24,13 @@ function App() {
 
   return (
     <div className="App">
-      <Titulo label="formmm en React 2024" />
+      <Titulo label="formmm en React 2027" />
       <Campo label="Nombras" onChange={(valor) => handleChange('nombres', valor)} />
       <Campo label="Apellidos" onChange={(valor) => handleChange('apellidos', valor)} />
       <Campo label="Tipo de docUMNE" onChange={(valor) => handleChange('tipoDocumento', valor)} />
       <Campo label="Nro. de Documentos" onChange={(valor) => handleChange('nroDocumento', valor)} />
       <Campo label="Nro. celular" onChange={(valor) => handleChange('telefono', valor)} />
-      <Campo label="Correo email" onChange={(valor) => handleChange('correo', valor)} />
+      <Campo label="Correo elctro" onChange={(valor) => handleChange('correo', valor)} />
       <Boton texto="Enviar" onClick = {handleSubmit} />
     </div>
   );
