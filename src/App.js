6a4ea47b4a6926ea17c36668792a5eabc60389d0
JorgeHaro <jharor@uni.pe>
2025-03-04 14:55:50 -0500

    otro mas

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
       <Campo label="Nro. de Documento" onChange={(valor) => handleChange('nroDocumento', valor)} />
       <Campo label="Nro. teléfono" onChange={(valor) => handleChange('telefono', valor)} />
       <Campo label="Correo electrónico" onChange={(valor) => handleChange('correo', valor)} />
-      <Boton texto="Enviar Ahora" onClick = {handleSubmit} />
+      <Boton texto="Enviar ya" onClick = {handleSubmit} />
     </div>
   );
 }
